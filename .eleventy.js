f0388009615f520c4cb91a72da09d2579dec1251
gericci <contact@gericci.me>
2024-11-03 10:10:53 +0100

    first commit

---
+++ .eleventy.js
@@ -0,0 +1,16 @@
+// const wavejs = require("wavesurfer.js");
+
+module.exports = async function(eleventyConfig) { 
+
+  eleventyConfig.setOutputDirectory("docs"); 
+
+  eleventyConfig.addShortcode("year", () => `${new Date().getFullYear()}`); 
+
+  eleventyConfig.addPassthroughCopy("css/*.*");
+  eleventyConfig.addPassthroughCopy("img/**/*.*");
+  eleventyConfig.addPassthroughCopy("js/*.js");
+  eleventyConfig.addPassthroughCopy("sw.js");
+  eleventyConfig.addPassthroughCopy("manifest.json");
+  eleventyConfig.addPassthroughCopy("robot.txt");
+ 
+};
---
+++ docs/sw.js
@@ -0,0 +1,39 @@
+const cacheName = 'lebellego-24';
+
+const filesToCache = [
+  '/',
+  '/css/vars.css',
+  '/css/AnekDevanagari-VariableFont_wdth,wght.ttf',
+  '/img/logo.svg'
+];
+
+self.addEventListener('install', function (e) {
+  e.waitUntil(
+    caches.open(cacheName).then(function (cache) {
+      return cache.addAll(filesToCache);
+    })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (key !== cacheName) {
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+self.addEventListener('fetch', function (e) {
+  e.respondWith(
+    fetch(e.request)
+      .catch(() => {
+        return caches.match(e.request).then(function (response) {
+          return response
+        })
+      })
+  );
+});
